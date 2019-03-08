900c32f55bec9ef06885d60917fad461aded81cb
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-08 14:55:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
+def batch_badge_creator(array)
+  array.each do {|name| puts "Hello, my name is #{name}."}
+  
 end
 
 def assign_rooms
